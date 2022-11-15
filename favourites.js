54d265c86288ea5cc158170991e674aec9673fea
Biswajeet Sahoo <97235820+Biswajeet78@users.noreply.github.com>
2022-11-15 13:05:08 +0530

    Add files via upload

---
+++ favourites.js
@@ -0,0 +1,67 @@
+//defnining variable to access the class
+var foodItem = document.querySelector('.search-results-container');
+
+//Favourites array to put values received from sessionStorag
+var favourites = [];
+if (JSON.parse(sessionStorage.getItem('favourites')) != null) {
+    favourites = [...JSON.parse(sessionStorage.getItem('favourites'))];
+}
+
+
+//Display favourites  insides favourites Container 
+function displayFavourites(favourites) {
+    foodItem.innerHTML = '';
+    for (var i = 0; i < favourites.length; i++) {
+        console.log(favourites[i]);
+        foodItem.innerHTML += ` <li class="search-results-li">
+        <div style="display:none;">${favourites[i].id}</div>
+        <img class="search-results-img" width=100 height=100 src="${favourites[i].img}"/>
+        <h2>${favourites[i].name}</h2>
+        <i class="fa fa-heart fa-3x red-heart"></i>
+    </li>`;
+    }
+
+}
+
+//display favourites meal that we have saved
+displayFavourites(favourites);
+
+
+//Removing from favourites list on clicking on red-heart 
+foodItem.addEventListener('click', function (e) {
+    console.log('hellow');
+    if (e.target.classList.contains('fa-heart')) {
+        if (e.target.classList.contains('red-heart')) {
+            e.target.classList.remove('red-heart');
+            var elm = e.target.parentElement.children[0].innerText;
+            favourites = favourites.filter(v => v.id != elm);
+        } else {
+            var __id = e.target.parentElement.children[0].innerText;
+            var __img = e.target.parentElement.children[1].getAttribute('src');
+            var __name = e.target.parentElement.children[2].innerText;
+            var obj = {
+                id: __id,
+                img: __img,
+                name: __name
+            }
+            e.target.classList.add('red-heart');
+            var elm = e.target.parentElement.children[0].innerText;
+            favourites.push(obj)
+        }
+        //Displaying the updated favourties after removal ;
+        displayFavourites(favourites);
+
+        //Storing the updated favourites;
+        sessionStorage.setItem('favourites', JSON.stringify(favourites));
+    }
+})
+
+
+//on click on the image of the elements in favourites container  redirect to meal Details page
+foodItem.addEventListener('click', function (e) {
+    if (e.target.classList.contains("search-results-img")) {
+        var value = e.target.parentElement.children[0].innerText;
+        sessionStorage.setItem('id', value);
+        location.href = 'mealDetailPage.html';
+    }
+})
---
+++ mealDetailPage.js
@@ -0,0 +1,28 @@
+// call the function to show detail of the meal we select
+// local session is used to store api data
+window.addEventListener("load", function (e) {
+    var id = sessionStorage.getItem('id');
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php/?i=${id}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.meals);
+            var imgContainer = document.querySelector('.meal-details');
+            imgContainer.innerHTML += `
+            <div class="meal-details-left">
+            <h1>${data.meals[0].strMeal}</h1>
+            <h2>Instructions :</h2>
+            <p class="instructions">${data.meals[0].strInstructions}</p>
+            <h2>Category :</h2>
+            <p>${data.meals[0].strCategory}</p>
+            <h2>Country :</h2>
+            <p>${data.meals[0].strArea}</p>
+            <h2>Youtube Tutorial :</h2>
+            <p><a style="color:white;text-decoration:underline"href=${data.meals[0].strYoutube}>${data.meals[0].strYoutube}</a></p>
+        </div>
+        <div class="meal-details-right">
+        <img  src=${data.meals[0].strMealThumb}></img>
+        </div>`;
+
+        })
+        .catch(err => console.log(err));
+})
